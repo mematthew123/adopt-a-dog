86cbf05241b80ef183e772d578ce372c6a551d63
Matthew Rhoads <mematthew123@gmail.com>
2023-03-08 17:04:47 -0700

    updated head tags and images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Adopt a furry friend today" />
+        <meta property="og:title" content="Adopt MT" key="title" />
+        <meta property="og:description" content="Adopt a furry friend today" />
+        <meta property="og:image" content="/flowerdog.jpg" />
+        <meta property="og:url" content="https://adopt-mt.vercel.app/" />
+      </Head>
       <body>
         <Main />
         <NextScript />
