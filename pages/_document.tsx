146efe3c066b091a46ecbf704d6e42708133757a
Matthew Rhoads <mematthew123@gmail.com>
2023-03-14 23:59:12 -0600

    more meta more beta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/favicon.ico" />
@@ -14,12 +14,15 @@ export default function Document() {
         <meta property="og:title" content="Adopt MT" key="title" />
         <meta property="og:description" content="Adopt a furry friend today" />
         <meta property="og:image" content="/flowerdog.jpg" />
-        <meta property="og:url" content="https://adopt-mt.vercel.app/" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://adoptmt.org/" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
