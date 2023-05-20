7ae868d0377fd66efa5c568dce57e524f59a53c5
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2023-05-20 00:03:59 -0600

    updated next config dfor sitemap

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+
 module.exports = {
   images: {
     domains: ['dl5zpyw5k3jeb.cloudfront.net', 'res.cloudinary.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ]
   },
 }
